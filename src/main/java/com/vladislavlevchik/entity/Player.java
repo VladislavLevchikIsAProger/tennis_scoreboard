a80f03db9f2f91d93e217f2e742bc08758e8855f
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-05-14 19:38:16 +0300

    feat: add a new entity Player

---
+++ src/main/java/com/vladislavlevchik/entity/Player.java
@@ -0,0 +1,23 @@
+package com.vladislavlevchik.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
