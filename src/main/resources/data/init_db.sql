e166f51dcb1772cb148f8567bef03c39f53f5c6b
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-05-14 19:36:59 +0300

    feat: add init_db.sql

---
+++ src/main/resources/data/init_db.sql
@@ -0,0 +1,29 @@
+INSERT into Players (id, name) values (1, 'L. KIVIC');
+INSERT into Players (id, name) values (2, 'K. LAMPOT');
+INSERT into Players (id, name) values (3, 'J. PORROT');
+INSERT into Players (id, name) values (4, 'G. GRIMBER');
+INSERT into Players (id, name) values (5, 'K. KUBERT');
+INSERT into Players (id, name) values (6, 'S. SAFIN');
+INSERT into Players (id, name) values (7, 'I. BORG');
+INSERT into Players (id, name) values (8, 'O. PORIS');
+INSERT into Players (id, name) values (9, 'A. GETIRM');
+INSERT into Players (id, name) values (10, 'S. POKOR');
+
+
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (1, 1, 10, 10 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (2, 2, 9, 2 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (3, 3, 8, 8 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (4, 4, 7, 4 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (5, 5, 6, 6 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (6, 6, 5, 5 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (7, 7, 4, 4 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (8, 8, 3, 3 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (9, 9, 2, 2 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (10, 10, 1, 1 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (11, 10, 2, 10 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (12, 10, 3, 3 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (13, 10, 4, 10 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (14, 10, 5, 5 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (15, 10, 6, 10 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (16, 9, 4, 4 );
+INSERT into Matches (id, player_one_id, player_two_id, winner_id) values (17, 8, 5, 5 );
