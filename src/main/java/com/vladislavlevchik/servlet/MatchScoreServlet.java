434c6ceb54c1dfd534dadaa07be30e38501455e9
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-05-31 12:45:53 +0300

    feat: new point in API (post)

--- src/main/java/com/vladislavlevchik/servlet/MatchScoreServlet.java
+++ src/main/java/com/vladislavlevchik/servlet/MatchScoreServlet.java
@@ -1,7 +1,8 @@
 package com.vladislavlevchik.servlet;
 
-import com.vladislavlevchik.dto.PlayerScoreResponseDto;
-import com.vladislavlevchik.entity.MatchScore;
+import com.vladislavlevchik.dto.MatchScoreDto;
+import com.vladislavlevchik.dto.PlayerScoreDto;
+import com.vladislavlevchik.service.MatchScoreCalculationService;
 import com.vladislavlevchik.service.OngoingMatchesService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,25 +11,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/match-score")
 public class MatchScoreServlet extends HttpServlet {
 
     private final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
+    private final MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uuid = req.getParameter("uuid");
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
 
-        MatchScore match = ongoingMatchesService.findById(uuid);
+        MatchScoreDto match = ongoingMatchesService.findById(uuid);
 
         req.setAttribute("match", match);
+        req.setAttribute("uuid", uuid);
 
         req.getRequestDispatcher("/match-score.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Sosi pisu");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        Long id = Long.parseLong(req.getParameter("playerId"));
+
+        MatchScoreDto match = ongoingMatchesService.findById(uuid);
+
+        matchScoreCalculationService.updateScore(match, id);
+
+        resp.sendRedirect("/match-score?uuid=" + uuid);
     }
 }
