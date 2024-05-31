d78a646ce9906099b13fe75ad2f06b0cee0e6a34
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-05-31 12:42:44 +0300

    feat: add PlayerScoreDto mapping

--- src/main/java/com/vladislavlevchik/mapper/MatchMapper.java
+++ src/main/java/com/vladislavlevchik/mapper/MatchMapper.java
@@ -1,9 +1,10 @@
 package com.vladislavlevchik.mapper;
 
 import com.vladislavlevchik.dto.MatchResponseDto;
+import com.vladislavlevchik.dto.PlayerScoreDto;
 import com.vladislavlevchik.entity.Match;
+import com.vladislavlevchik.entity.Player;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
 import org.modelmapper.convention.MatchingStrategies;
 
 public class MatchMapper {
@@ -13,13 +14,20 @@ public class MatchMapper {
         MODEL_MAPPER = new ModelMapper();
         MODEL_MAPPER.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
-        MODEL_MAPPER.typeMap(Match.class, MatchResponseDto.class).
-                addMapping(src -> src.getPlayerTwo().getName(), MatchResponseDto::setPlayerOne).
-                addMapping(src -> src.getPlayerTwo().getName(), MatchResponseDto::setPlayerTwo).
-                addMapping(src -> src.getWinner().getName(), MatchResponseDto::setWinnerName);
+        MODEL_MAPPER.typeMap(Match.class, MatchResponseDto.class)
+                .addMapping(src -> src.getPlayerTwo().getName(), MatchResponseDto::setPlayerOne)
+                .addMapping(src -> src.getPlayerTwo().getName(), MatchResponseDto::setPlayerTwo)
+                .addMapping(src -> src.getWinner().getName(), MatchResponseDto::setWinnerName);
+
+        MODEL_MAPPER.typeMap(Player.class, PlayerScoreDto.class)
+                .addMapping(Player::getName, PlayerScoreDto::setPlayerName);
     }
 
     public static MatchResponseDto convertToDto(Match match) {
         return MODEL_MAPPER.map(match, MatchResponseDto.class);
+    }
+
+    public static PlayerScoreDto convertToDto(Player player) {
+        return MODEL_MAPPER.map(player, PlayerScoreDto.class);
     }
 }
