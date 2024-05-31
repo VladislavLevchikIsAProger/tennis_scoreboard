63e2367b58f6e7ca63a3eac981381375071d6da1
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-05-31 16:06:17 +0300

    feat: all necessary calculations have been added

--- src/main/java/com/vladislavlevchik/service/MatchScoreCalculationService.java
+++ src/main/java/com/vladislavlevchik/service/MatchScoreCalculationService.java
@@ -10,25 +10,67 @@ public class MatchScoreCalculationService {
         PlayerScoreDto playerTwo = matchScoreDto.getPlayerTwo();
 
         if (playerOne.getId().equals(id)) {
-            handleGameResult(playerOne, playerTwo);
+            determineScoringMethod(playerOne, playerTwo);
         } else {
-            handleGameResult(playerTwo, playerOne);
+            determineScoringMethod(playerTwo, playerOne);
+        }
+    }
+
+    private void determineScoringMethod(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
+        if (pointWinner.getGames() == 6 && opponent.getGames() == 6) {
+            playTieBreak(pointWinner, opponent);
+        } else {
+            handleGameResult(pointWinner, opponent);
+            handleSetResult(pointWinner, opponent);
         }
     }
 
     private void handleGameResult(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
-        if (pointWinner.getPoints() == 3 && opponent.getPoints() < 3) {
+        int pointWinnerPoints = pointWinner.getPoints();
+        int opponentPoints = opponent.getPoints();
+
+        if (pointWinnerPoints == 3 && opponentPoints < 3) {
             pointWinner.wonGame();
 
             clearPointsFromPlayers(pointWinner, opponent);
-        } else if (pointWinner.getPoints() >= 3 && opponent.getPoints() >= 3) {
-            if (pointWinner.getPoints() > opponent.getPoints()) {
-                pointWinner.wonGame();
+        } else if (pointWinnerPoints >= 3 && opponentPoints >= 3) {
+            playOnMoreOrLess(pointWinner, opponent);
+        } else {
+            pointWinner.wonPoint();
+        }
+    }
 
-                clearPointsFromPlayers(pointWinner, opponent);
-            } else {
-                pointWinner.wonPoint();
-            }
+    private void handleSetResult(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
+        int pointWinnerGames = pointWinner.getGames();
+        int opponentGames = opponent.getGames();
+
+        if (pointWinnerGames >= 6 && pointWinnerGames - opponentGames >= 2) {
+            pointWinner.wonSet();
+
+            clearGamesFromPlayers(pointWinner, opponent);
+        }
+    }
+
+    private void playTieBreak(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
+        int pointWinnerPoints = pointWinner.getPoints();
+        int opponentPoints = opponent.getPoints();
+
+        if (pointWinnerPoints >= 6 && pointWinnerPoints - opponentPoints >= 1) {
+            pointWinner.wonSet();
+
+            clearGamesFromPlayers(pointWinner, opponent);
+            clearPointsFromPlayers(pointWinner, opponent);
+        } else {
+            pointWinner.wonPoint();
+        }
+    }
+
+
+    private void playOnMoreOrLess(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
+        if (pointWinner.getPoints() > opponent.getPoints()) {
+            pointWinner.wonGame();
+
+            clearPointsFromPlayers(pointWinner, opponent);
         } else {
             pointWinner.wonPoint();
         }
@@ -37,6 +79,11 @@ public class MatchScoreCalculationService {
     private void clearPointsFromPlayers(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
         pointWinner.clearPoints();
         opponent.clearPoints();
+    }
+
+    private void clearGamesFromPlayers(PlayerScoreDto pointWinner, PlayerScoreDto opponent) {
+        pointWinner.clearGames();
+        opponent.clearGames();
     }
 
 }
