a9b686e1317939a2ddced0a8b32d3c920ab681ed
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-05-31 12:38:33 +0300

    feat: add MatchScoreDto

---
+++ src/main/java/com/vladislavlevchik/dto/MatchScoreDto.java
@@ -0,0 +1,19 @@
+package com.vladislavlevchik.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MatchScoreDto {
+
+    PlayerScoreDto playerOne;
+
+    PlayerScoreDto playerTwo;
+
+}
+
