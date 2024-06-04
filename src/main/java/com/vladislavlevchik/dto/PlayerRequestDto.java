7fc49c943502d2fb9c8599e8272ec402e1a8188c
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-06-04 16:26:00 +0300

    feat: add PlayerRequestDto

---
+++ src/main/java/com/vladislavlevchik/dto/PlayerRequestDto.java
@@ -0,0 +1,15 @@
+package com.vladislavlevchik.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlayerRequestDto {
+
+    String name;
+
+}
