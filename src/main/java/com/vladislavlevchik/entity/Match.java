817850cd9d69f045f09be2f97bff12a96f376270
Vladislav Levchik Home <vlad.levchik.ares@icloud.com>
2024-06-04 12:48:29 +0300

    fix: replaced @Data annotation

--- src/main/java/com/vladislavlevchik/entity/Match.java
+++ src/main/java/com/vladislavlevchik/entity/Match.java
@@ -1,12 +1,11 @@
 package com.vladislavlevchik.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
