7ebd4a66c1cbd36ba8e774adc6ea8254888e6870
Vladislav Levchik Work <vlad.levchik.ares@icloud.com>
2024-06-14 11:38:50 +0300

    style: unnecessary code removed

--- src/main/java/com/vladislavlevchik/service/PlayerPersistenceService.java
+++ src/main/java/com/vladislavlevchik/service/PlayerPersistenceService.java
@@ -3,7 +3,6 @@ package com.vladislavlevchik.service;
 import com.vladislavlevchik.dto.PlayerRequestDto;
 import com.vladislavlevchik.entity.Player;
 import com.vladislavlevchik.repository.PlayerRepository;
-import com.vladislavlevchik.utils.MapperUtil;
 
 import static com.vladislavlevchik.utils.MapperUtil.convertToEntity;
 
